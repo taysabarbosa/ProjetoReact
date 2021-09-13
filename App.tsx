0898a498ee8f3bcc9dc00b068ddbfa0d306bddb6
Taysa <taysabarbosa2015@gmail.com>
2021-09-13 20:05:47 -0300

    Novos projetos mobile

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_500Medium,
+        Rajdhani_500Medium,
+        Rajdhani_700Bold
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />
+
+    }
+
+    return (
+        <Background>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Home />
+        </Background>
+    );
+}
